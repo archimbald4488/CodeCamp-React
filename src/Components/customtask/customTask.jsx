9df4d287f720d76b1e1f890e41b0b9285ca5f730
aRakkolainen <aino.rakkolainen@gmail.com>
2024-01-11 09:14:59 +0200

    Update customTask.jsx

--- src/Components/customtask/customTask.jsx
+++ src/Components/customtask/customTask.jsx
@@ -5,6 +5,10 @@ export const customTask = () => {
     <>
     <div className="container-s" color="#FAFAFA">
         <h2>Add new task</h2>
+        <input type="text" placeholder="Write name for new task.."/>
+        <h2>Task Description</h2>
+        <input type="text" placeholder="Write new task.." />
+        <h2>Weather criteria</h2>
         <Button variant="secondary">Cancel</Button>
         <Button variant="primary">Done</Button>
     </div>
