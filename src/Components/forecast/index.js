6fef61b341170d9ea2c300d2c4104023bf9b7727
muhammadhamzamumtaz <144484040+muhammadhamzamumtaz@users.noreply.github.com>
2024-01-14 15:13:46 +0200

    updated structure

--- src/Components/forecast/index.js
+++ src/Components/forecast/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import './style.css';
-import HourlyForecast from './app.jsx';
-
-const App = () => (
-  <div className="App">
-    <HourlyForecast />
-  </div>
-);
-
-export default App;
+export { HourlyForecast } from "./hourlyforecast.jsx";
